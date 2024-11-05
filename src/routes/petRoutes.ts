b7ed4ec4d2f15f48695d989e6518c21cb02a5751
Yaqi Fang <cfang322@gmail.com>
2024-11-05 18:17:29 -0500

    Create petController.ts for getting all pets and set up route

--- src/routes/petRoutes.ts
+++ src/routes/petRoutes.ts
@@ -3,14 +3,20 @@
 // Define Basic Routes
 // Import express and create a router:
 import express from 'express';
+import { getAllPets } from '../controllers/petController';
+
 const router = express.Router();
 
 // Add routes. For example, add a route to get all pets:
-router.get('/pets', (req, res) => {
+// router.get('/pets', (req, res) => {
     // log middleware, add log statement to check if the route handler is reached
-    console.log('Request received for /pets');
-    res.json({ message: 'List of all pets' });
-});
+    // console.log('Request received for /pets');
+//     res.json({ message: 'List of all pets' });
+// });
+
+// Add route to get all pets
+// Use the imported getAllPets function
+router.get('/pets', getAllPets); 
 
 // Export the router
 export default router;  
