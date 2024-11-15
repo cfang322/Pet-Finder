dcfb54dfc47b62164f1766c80e2e4dfe96c90d4a
Yaqi Fang <cfang322@gmail.com>
2024-11-15 12:45:28 -0500

    space

--- src/controllers/petController.ts
+++ src/controllers/petController.ts
@@ -3,4 +3,4 @@ import { Request, Response } from 'express';
 export const getAllPets = (req: Request, res: Response) => {
     // console.log('Request received for getAllPets controller');
     res.json({ message: 'List of all pets from controller' });
-} ;
+} ;  
